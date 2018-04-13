431fd082e74ff16f47f9c913b15bbc3feb5c4505
Mingzhang <redondoself@gmail.com>
2018-04-13 01:07:01 -0400

    Avoid using local imports

--- machinary/enzyme/re_enzyme.go
+++ machinary/enzyme/re_enzyme.go
@@ -4,27 +4,29 @@ import (
 	"errors"
 	"regexp"
 
-	"../../sequence"
+	"BioGo/sequence"
 )
 
-type RE_Site struct {
+// ReSite type
+type ReSite struct {
 	p int
 	s string
 }
 
-func (s RE_Site) seq() string {
+func (s ReSite) seq() string {
 	return s.s
 }
 
+// common enzymes
 var (
-	BamHI   = RE_Site{1, "GGATCC"}
-	HindIII = RE_Site{1, "AAGCTT"}
-	KpnI    = RE_Site{5, "GGTACC"}
-	NdeI    = RE_Site{2, "CATATG"}
-	XhoI    = RE_Site{1, "CTCGAG"}
+	BamHI   = ReSite{1, "GGATCC"}
+	HindIII = ReSite{1, "AAGCTT"}
+	KpnI    = ReSite{5, "GGTACC"}
+	NdeI    = ReSite{2, "CATATG"}
+	XhoI    = ReSite{1, "CTCGAG"}
 )
 
-var dict = make(map[string]RE_Site)
+var dict = make(map[string]ReSite)
 
 func init() {
 	dict["BamHI"] = BamHI
@@ -34,36 +36,37 @@ func init() {
 	dict["XhoI"] = XhoI
 }
 
-type RE_Enzyme struct {
+// ReEnzyme type
+type ReEnzyme struct {
 	Name            string
 	RecognitionSite *regexp.Regexp
 }
 
-// NewRE_EnzymeFromName create an new RE_Enzyme from the enzyme name
-func NewRE_EnzymeFromName(name string) (*RE_Enzyme, error) {
-	var s RE_Site
+// NewReEnzymeFromName create an new ReEnzyme from the enzyme name
+func NewReEnzymeFromName(name string) (*ReEnzyme, error) {
+	var s ReSite
 	var ok bool
 	if s, ok = dict[name]; !ok {
 		return nil, errors.New("enzyme not in the pre-built collection, please try NewRE_EnzymeFromPattern instead")
 	}
-	return &RE_Enzyme{name, regexp.MustCompile(s.seq())}, nil
+	return &ReEnzyme{name, regexp.MustCompile(s.seq())}, nil
 }
 
-// NewRE_EnzymeFromPattern create an new RE_Enzyme from the recognition site
-func NewRE_EnzymeFromPattern(pat string) (*RE_Enzyme, error) {
+// NewReEnzymeFromPattern create an new ReEnzyme from the recognition site
+func NewReEnzymeFromPattern(pat string) (*ReEnzyme, error) {
 	re, err := regexp.Compile(pat)
 	if err != nil {
 		return nil, err
 	}
-	return &RE_Enzyme{RecognitionSite: re}, nil
+	return &ReEnzyme{RecognitionSite: re}, nil
 }
 
 // SearchDNA return the index of all the matched region
-func (enz RE_Enzyme) SearchDNA(dna *sequence.DNA) [][]int {
+func (enz ReEnzyme) SearchDNA(dna *sequence.DNA) [][]int {
 	return enz.RecognitionSite.FindAllStringIndex(dna.Seq(), -1)
 }
 
 // SearchRawSequence search a raw string
-func (enz RE_Enzyme) SearchRawSequence(str string) [][]int {
+func (enz ReEnzyme) SearchRawSequence(str string) [][]int {
 	return enz.RecognitionSite.FindAllStringIndex(str, -1)
 }
