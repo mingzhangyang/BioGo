c1818e074debbaeac04ffd89c758f46c06d4f088
mingzhang <redondoself@gmail.com>
2018-04-18 16:00:03 -0400

    layout the hmm model

--- algorithm/hmm.go
+++ algorithm/hmm.go
@@ -1,2 +1,29 @@
 package algorithm
 
+// This file try to implement HMM using golang
+
+// Probability type describing the probability of an event
+type Probability struct {
+	event       string
+	probability float64
+}
+
+// HMM type
+type HMM struct {
+	States                []string
+	Observations          []string
+	TransistionProbablity map[string][]Probability
+	EmissionProbability   map[string][]Probability
+}
+
+// Monitor method to calculate the probability of a state given observation
+// the arguments are a list of CONSECUTIVE observations
+func (h *HMM) Monitor(obs ...string) {
+
+}
+
+// MostLikelyPath method calculate the most likely path through which
+// the hidden states go
+func (h *HMM) MostLikelyPath(seq []string) {
+
+}
