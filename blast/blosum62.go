4f2eb98b12c6e577ad508e21bac559defea2fcde
Mingzhang <redondoself@gmail.com>
2019-01-02 23:41:21 -0500

    blosum62

--- blast/blosum62.go
+++ blast/blosum62.go
@@ -6,6 +6,8 @@ import (
 	"fmt"
 )
 
+const aa = "ARNDCQEGHILKMFPSTWYV*"
+
 const s = `   A  R  N  D  C  Q  E  G  H  I  L  K  M  F  P  S  T  W  Y  V  B  Z  X  *
 A  4 -1 -2 -2  0 -1 -1  0 -2 -1 -1 -1 -1 -2 -1  1  0 -3 -2  0 -2 -1  0 -4 
 R -1  5  0 -2 -3  1  0 -2  0 -3 -2  2 -1 -3 -2 -1 -1 -3 -2 -3 -1  0 -1 -4 
@@ -62,3 +64,46 @@ func prep(s string) map[string]int {
 
 // BLOSUM62 exported
 var BLOSUM62 = prep(s)
+
+// BlosumScore compute the score between two strings
+// The length of s1 and s2 should be equal, and space should be represented as *
+func BlosumScore(s1, s2 string) int {
+	if len(s1) != len(s2) {
+		panic("Two strings with the same length are expected.")
+	}
+	s1 = strings.ToUpper(s1)
+	s2 = strings.ToUpper(s2)
+	var res int
+	for i, max := 0, len(s1); i < max; i++ {
+		key := string([]byte{s1[i], s2[i]})
+		if v, ok := BLOSUM62[key]; ok {
+			res += v
+		} else {
+			panic(fmt.Sprintf("Invalid combination of %s and %s\n", string(s1[i]), string(s2[i])))
+		}
+	}
+	return res
+}
+
+// SimilarWords produce a list of similar words with score higher than threshold
+// This function works only for words with length = 3
+func SimilarWords(s string, t float64) []string {
+	res := make([]string, 0)
+	word := make([]byte, 3)
+	for i := 0; i < 21; i++ {
+		for j := 0; j < 21; j++ {
+			for k := 0; k < 21; k++ {
+				word[0] = aa[i]
+				word[1] = aa[j]
+				word[2] = aa[k]
+				v1 := BLOSUM62[string([]byte{s[0], aa[i]})]
+				v2 := BLOSUM62[string([]byte{s[1], aa[j]})]
+				v3 := BLOSUM62[string([]byte{s[2], aa[k]})]
+				if float64(v1) + float64(v2) + float64(v3) >= t {
+					res = append(res, string(word))
+				}
+			}
+		}
+	}
+	return res
+}
